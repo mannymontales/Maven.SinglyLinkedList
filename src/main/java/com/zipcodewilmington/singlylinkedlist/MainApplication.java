da4fb6538e667985fc3dd8174255260e1fcf9fb8
Manny <manny@Zipcoders-MacBook-Pro-58.local>
2023-08-02 10:06:33 -0400

    what i have so far before lecture on singly linked list

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -4,4 +4,9 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class MainApplication {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
